648e4ef1e186ea62679f7b6c562eae406884af35
Md Shahajahan Ali <msacse1@gmail.com>
2025-04-22 09:53:22 +0600

    some modify

--- application/views/admin/schedule/index.php
+++ application/views/admin/schedule/index.php
@@ -179,7 +179,7 @@ $(document).ready(function() {
     // get designations
     $('[data-plugin="select_hrm"]').select2($(this).attr('data-options'));
     $('[data-plugin="select_hrm"]').select2({
-        width: '100%'
+        width: '80%'
     });
 });
 </script>
